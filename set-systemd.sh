39914e1d11979d05d7a3d157d90928f18d7d5a0b
Wildy Sheverando <126782054+wildy8283@users.noreply.github.com>
2023-03-27 08:03:09 +0700

    Update set-systemd.sh

--- set-systemd.sh
+++ set-systemd.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# By Wildy Sheverando
+# Git: https://github.com/wildy8283/GPT-Bot
+# ==================================================
 
 export pyt3=$(which python3)
+export NJSPATH=$(which node)
 export MAIN_PATH=$(pwd)
 
 echo "[Unit]
@@ -10,7 +14,7 @@ After=network.target
 [Service]
 Type=simple
 User=root
-WorkingDirectory=/program/GPT-Bot/WhatsApp
+WorkingDirectory=${MAIN_PATH}
 ExecStart=${pyt3} ${MAIN_PATH}/Server/server.py
 Restart=always
 RestartSec=3
@@ -18,3 +22,29 @@ RestartSec=3
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/gpt-api.service
 
+# >> Restarting service
+systemctl daemon-reload
+systemctl stop gpt-api
+systemctl enable gpt-api
+systemctl restart gpt-api
+
+echo "[Unit]
+Description=GPT-Bot | WhatsApp
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=${MAIN_PATH}
+ExecStart=${NJSPATH} ${MAIN_PATH}/WhatsApp/index.js
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/gpt-bot-wa.service
+
+# >> Restarting service
+systemctl daemon-reload
+systemctl stop gpt-bot-wa
+systemctl enable gpt-bot-wa
+systemctl restart gpt-bot-wa
